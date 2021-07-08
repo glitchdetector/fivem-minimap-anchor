d0ff56dbe3955d1ec95cd394223bdace74edb662
-del1an <darkyzzx@gmail.com>
2021-07-08 19:39:11 +0300

    Update

--- MINIANCHOR.lua
+++ MINIANCHOR.lua
@@ -14,18 +14,20 @@
 function GetMinimapAnchor()
     -- Safezone goes from 1.0 (no gap) to 0.9 (5% gap (1/20))
     -- 0.05 * ((safezone - 0.9) * 10)
-    local safezone = GetSafeZoneSize()
-    local safezone_x = 1.0 / 20.0
-    local safezone_y = 1.0 / 20.0
-    local aspect_ratio = GetAspectRatio(0)
+	local aspect_ratio = GetAspectRatio(0)
     local res_x, res_y = GetActiveScreenResolution()
     local xscale = 1.0 / res_x
     local yscale = 1.0 / res_y
     local Minimap = {}
+
+	SetScriptGfxAlign(string.byte('L'), string.byte('B'))
+	local gfx_x, gfx_y = GetScriptGfxPosition(0, 0)
+	ResetScriptGfxAlign()
+
     Minimap.width = xscale * (res_x / (4 * aspect_ratio))
     Minimap.height = yscale * (res_y / 5.674)
-    Minimap.left_x = xscale * (res_x * (safezone_x * ((math.abs(safezone - 1.0)) * 10)))
-    Minimap.bottom_y = 1.0 - yscale * (res_y * (safezone_y * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.left_x = gfx_x
+    Minimap.bottom_y = gfx_y
     Minimap.right_x = Minimap.left_x + Minimap.width
     Minimap.top_y = Minimap.bottom_y - Minimap.height
     Minimap.x = Minimap.left_x
